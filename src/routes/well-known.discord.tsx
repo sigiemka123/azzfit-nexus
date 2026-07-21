fd679c9411e08290c51d2b94e134dcee4967bc8e
sigiemka123 <sniatalamaciek@gmail.com>
2026-07-22 00:18:29 +0200

    Update well-known.discord.tsx

--- src/routes/well-known.discord.tsx
+++ src/routes/well-known.discord.tsx
@@ -1,1 +1,12 @@
+import { createFileRoute } from '@tanstack/react-router'
 
+export const Route = createFileRoute('/well-known/discord')({
+  loader: () => {
+    return new Response('dh=bb4bfda1082eda501bd465a20c87240e84a...', {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    })
+  },
+})
